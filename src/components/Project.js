73d88f7c7c6b3d8d1cf56798bc7fff933c9f5b37
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-08 13:09:41 -0700

    added project to project section and fixed styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,9 @@ import "./Project.css";
 const Project = ({ title, imageUrl, liveUrl, repoUrl, techStack }) => {
   return (
     <div className="project" style={{ backgroundImage: `url(${imageUrl})` }}>
-      <a href={liveUrl} target="_blank" rel="noopener noreferrer">
-        <div className="image-overlay">
-          <h3>{title}</h3>
-        </div>
-      </a>
       <div className="project-details">
         <a href={liveUrl} target="_blank" rel="noopener noreferrer">
-          Live Project
+          <h3>{title}</h3>
         </a>
         <span>{techStack}</span>
         <a href={repoUrl} target="_blank" rel="noopener noreferrer">
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -28,16 +28,24 @@ const projectData = [
     description: "A health quest app.",
     imageUrl: require("../images/HealthQuest.png"),
     liveUrl: "https://groupfour-healthquest-5415f1d2dd04.herokuapp.com/",
-    repoUrl: "https://github.com/group-four/healthquest",
+    repoUrl: "https://github.com/Jenina52112/HealthQuest-App",
     techStack: "JS/Node/Express/MYSQL",
   },
   {
     title: "Parks Planner",
     description: "A parks planner app.",
     imageUrl: require("../images/yosemite.png"),
-    liveUrl: "https://adamh1223.github.io/group4-class-project-1/",
+    liveUrl: "https://adamh1223.github.io/parks-planner/",
     repoUrl: "https://github.com/adamh1223/group4-class-project-1",
-    techStack: "HTML/CSS",
+    techStack: "HTML/CSS/JS",
+  },
+  {
+    title: "Code-Quiz",
+    description: "A coding quiz app.",
+    imageUrl: require("../images/Quiz-Code.png"),
+    liveUrl: "https://david-nosach.github.io/Code-Quiz/",
+    repoUrl: "https://github.com/David-Nosach/Code-Quiz",
+    techStack: "HTML/CSS/JS",
   },
 ];
 
